ad65ad1cb1dae371916feaf667a7eba3d691b51c
obywatel <k.kaluzny141@gmail.com>
2019-02-11 01:38:08 +0100

    Set-up project

---
+++ src/main/java/org/demo/solr/solrtwitterdemo/SolrTwitterDemoApplication.java
@@ -0,0 +1,14 @@
+package org.demo.solr.solrtwitterdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolrTwitterDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolrTwitterDemoApplication.class, args);
+    }
+
+}
+
