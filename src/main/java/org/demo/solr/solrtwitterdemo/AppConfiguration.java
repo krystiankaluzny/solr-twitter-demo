da3de3f07e2e56b9b06a260d74aafaac509ede56
obywatel <k.kaluzny141@gmail.com>
2019-02-19 04:34:50 +0100

    Solr embedded server

--- src/main/java/org/demo/solr/solrtwitterdemo/AppConfiguration.java
+++ src/main/java/org/demo/solr/solrtwitterdemo/AppConfiguration.java
@@ -1,11 +1,18 @@
 package org.demo.solr.solrtwitterdemo;
 
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
 import org.asynchttpclient.AsyncHttpClient;
 import org.asynchttpclient.DefaultAsyncHttpClient;
 import org.asynchttpclient.DefaultAsyncHttpClientConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
 
 @Configuration
 class AppConfiguration {
@@ -23,5 +30,12 @@ class AppConfiguration {
                 .setRequestTimeout(requestTimeout)
                 .build()
         );
+    }
+
+    @Bean
+    SolrClient solrClient() throws URISyntaxException {
+        URL url = Thread.currentThread().getContextClassLoader().getResource("solr");
+        Path path = new File(url.toURI()).toPath();
+        return new EmbeddedSolrServer(path, "tweets");
     }
 }
