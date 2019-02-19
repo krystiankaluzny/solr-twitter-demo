04607f1f98e74d68ab8bb26cbaa6ac716ab32f72
obywatel <k.kaluzny141@gmail.com>
2019-02-19 06:34:13 +0100

    Write some tests

--- src/test/java/org/demo/solr/solrtwitterdemo/SolrTwitterDemoApplicationTests.java
+++ src/test/java/org/demo/solr/solrtwitterdemo/SolrTwitterDemoApplicationTests.java
@@ -1,17 +1,63 @@
 package org.demo.solr.solrtwitterdemo;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SolrTwitterDemoApplicationTests {
 
+    @Autowired
+    private TweetRepository dbTweetRepository;
+
+    @Autowired
+    private TweetRepository solrTweetRepository;
+
     @Test
-    public void contextLoads() {
+    public void searchingOnSolrShouldReturnTweetsWithMatchingWords() {
+
+        solrTweetRepository.add(createTweet("Some text with abc"));
+        solrTweetRepository.add(createTweet("Some aBc int text"));
+        solrTweetRepository.add(createTweet("And abcde"));
+
+        List<Tweet> tweets = solrTweetRepository.findTweetsContainingPhrase("abc");
+
+        Assertions.assertThat(tweets)
+            .hasSize(2)
+            .extracting(Tweet::getText)
+            .containsExactlyInAnyOrder(
+                "Some text with abc",
+                "Some aBc int text");
     }
 
+    @Test
+    public void searchingOnRDBMSShouldReturnAllTweetsContainingPhrase() {
+
+        dbTweetRepository.add(createTweet("Some text with abc"));
+        dbTweetRepository.add(createTweet("Some aBc int text"));
+        dbTweetRepository.add(createTweet("And abcde"));
+
+        List<Tweet> tweets = dbTweetRepository.findTweetsContainingPhrase("abc");
+
+        Assertions.assertThat(tweets)
+            .hasSize(3)
+            .extracting(Tweet::getText)
+            .containsExactlyInAnyOrder(
+                "Some text with abc",
+                "Some aBc int text",
+                "And abcde");
+    }
+
+    private Tweet createTweet(String text) {
+        Tweet tweet = new Tweet();
+        tweet.setText(text);
+        return tweet;
+    }
 }
 
